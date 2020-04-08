cdee63ace9ee78ca22423ee81263d9db395f8129
Juan C <juanceballos@pursuit.org>
2020-04-08 00:38:19 -0400

    fc

---
+++ KVO-Intro.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+// KVO - Key Value Observing
+// KVO is part of the observer pattern similar to Notif Center
+// One to Many pattern relationship unlike del One to One
+
+// In delegation pattern
+// class VC: ViewController {}
+// eg tableView.datasource = self
+
+// KVO is an objc runtime API, some essentials required
+// Object only works with reference types so on classes
+// 1. The object being observed needs to be a class
+// 2. The class needs to inherit from NSObject, it's top abstract class in Objective-C
+// 3. Any property being marked for observation needs to be prefixed with @objc dynamic. Dynamic means
+//    that the property is being dynamically dispatched (at runtime the compiler verifies the
+//    underlying property)
+// In swift types are statiscally dispatched which means they are checked at compile time vs objective c which
+// dynamically dispatched and check at runtime
+
+// dog class(class being observed) - will have a property to be observed
+@objc class Dog: NSObject { // inherits from NSObject, for protocols conforms
+    var name: String
+    @objc dynamic var age: Int // age is observable property
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    // Dog is the subject
+}
+
+// observer class one
+class DogWalker {
+    let dog: Dog
+    var birthdayObservation: NSKeyValueObservation? // a handle for the property being observed
+    
+    init(dog: Dog)  {
+        self.dog = dog
+    }
+    
+    private func configureBirthdayObservation() {
+        // now can do observation syntax
+        // \. = keypass for kvo
+        // dog value, and change property will have values for age new and old
+        birthdayObservation = dog.observe(\.age, options: [.old, .new], changeHandler: { (dog, change) in
+            <#code#>
+        })
+    }
+}
+
+// observer class 2
+class DogGroomer    {
+    
+}
+
+
